e806f4e0a0a8a229e28d26a4c11c814f50e35245
Sifur Rahaman <sifurrshishir@gmail.com>
2020-12-27 15:30:10 +0600

    Started project with basic settings and app_login models

---
+++ My_Ecommerce_Project/urls.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from django.contrib.staticfiles.urls import static, staticfiles_urlpatterns
+
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("", include("app_shop.urls")),
+    path("account/", include("app_login.urls")),
+]
+
+
+urlpatterns += staticfiles_urlpatterns()
+urlpatterns += static(settings.MEDIA_URL, doccument_root=settings.MEDIA_ROOT)
