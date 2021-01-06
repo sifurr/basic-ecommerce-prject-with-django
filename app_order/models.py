b015f9a762c516839bb45543e95ce9e4b39da586
Sifur Rahaman <saifurrshishir@gmail.com>
2021-01-06 12:32:35 +0600

    Cart template done without remove and continuw shopping options

--- app_order/models.py
+++ app_order/models.py
@@ -5,6 +5,8 @@ from django.conf import settings
 from app_shop.models import Product
 
 # Create your models here.
+
+
 class Cart(models.Model):
     user = models.ForeignKey(
         settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="cart"
@@ -30,16 +32,16 @@ class Cart(models.Model):
 
 # sub total = 950tk will be passed to Order table or model
 
-
 class Order(models.Model):
     orderItems = models.ManyToManyField(Cart)
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL,
+                             on_delete=models.CASCADE)
     ordered = models.BooleanField(default=False)
     created = models.DateTimeField(auto_now_add=True)
     paymentId = models.CharField(max_length=264, blank=True, null=True)
     orderId = models.CharField(max_length=200, blank=True, null=True)
 
-    def get_total(self):
+    def get_totals(self):
         total = 0
         for order_item in self.orderItems.all():
             total += float(order_item.get_total())
--- app_order/urls.py
+++ app_order/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
+from app_order import views
+
 
 app_name = "app_order"
 
 
-urlpatterns = []
+urlpatterns = [
+    path("add/<pk>", views.add_to_cart, name="add"),
+    path("cart/", views.cart_view, name='cart')
+]
